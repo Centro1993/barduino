9395cb32de3107594a2c25f5775cfcb1726261da
Jonas Leitner <jonas.leitner93@gmail.com>
2022-06-11 18:07:34 +0200

    removed mutexes

--- logic/barkeeper.go
+++ logic/barkeeper.go
@@ -4,7 +4,6 @@ import (
 	"barduino/gpio"
 	"barduino/models"
 	"errors"
-	"sync"
 )
 
 const SERVING_SIZE_IN_ML uint = 200
@@ -14,19 +13,13 @@ var drinkStatus models.DrinkStatus = models.DrinkStatus{
 	CurrentlyServing: false,
 	IngredientEmpty: false,
 }
-var drinkStatusMutex sync.RWMutex = sync.RWMutex{}
-
 // Pump channels
 var runningPumps map[uint]chan models.PumpStatus
-var runningPumpsMutex sync.RWMutex = sync.RWMutex{}
 
 func PourDrink (drink models.Drink) error {
-	runningPumpsMutex.Lock()
 	runningPumps = make(map[uint]chan models.PumpStatus)
-	runningPumpsMutex.Unlock()
 	recipe := drink.Recipe
 
-	drinkStatusMutex.Lock()
 	if drinkStatus.CurrentlyServing {
 		return errors.New("cannot pour drink, currently serving another drink")
 	}
@@ -36,7 +29,6 @@ func PourDrink (drink models.Drink) error {
 
 	drinkStatus.CurrentlyServing = true
 	drinkStatus.IngredientEmpty = false 
-	drinkStatusMutex.Unlock()
 
 	pumpInstructions := recipe.ConvertRecipeToPumpInstructions(SERVING_SIZE_IN_ML)
 	for _, pumpInstruction := range pumpInstructions {
@@ -49,13 +41,11 @@ func PourDrink (drink models.Drink) error {
 }
 
 func CancelDrink () error {
-	drinkStatusMutex.Unlock()
 	if !drinkStatus.CurrentlyServing {
 		return errors.New("cannot cancel drink, not currently serving")
 	}
 
 	drinkStatus.CurrentlyServing = false
-	drinkStatusMutex.Lock()
 
 	// As a safety Measure, manually disable all pumps
 	var pumps []models.Pump
@@ -68,12 +58,10 @@ func CancelDrink () error {
 }
 
 func monitorPump(pump models.Pump) error {
-	runningPumpsMutex.Lock()
 	if runningPumps[pump.ID] == nil {
 		return errors.New("pump has no open channel and cannot be monitored")
 	}
 	currentChannel := runningPumps[pump.ID]
-	runningPumpsMutex.Unlock()
 
 	// tell pump to start
 	currentChannel <- models.PumpStatus{
@@ -88,19 +76,15 @@ func monitorPump(pump models.Pump) error {
 	for pumpStatus := range currentChannel {
 		// this stops all pumps if this pump runs dry
 		if pumpStatus.IngredientEmpty {
-			drinkStatusMutex.Lock()
 			drinkStatus.IngredientEmpty = true
 			// acknowledge the report
 			currentChannel <- models.PumpStatus{
 				IngredientEmpty: drinkStatus.IngredientEmpty,
 				CurrentlyServing: drinkStatus.CurrentlyServing,
 			}
-			drinkStatusMutex.Unlock()
-
 			// continously ask the pump if the ingredient has been refilled
 			for dryPumpStatus := range currentChannel {
 				// if the pump has been refilled, inform the other pumpMonitors
-				drinkStatusMutex.Unlock()
 				if !dryPumpStatus.IngredientEmpty {
 					drinkStatus.IngredientEmpty = false
 					// and continue pumping
@@ -111,26 +95,21 @@ func monitorPump(pump models.Pump) error {
 					IngredientEmpty: drinkStatus.IngredientEmpty,
 					CurrentlyServing: drinkStatus.CurrentlyServing,
 				}
-				drinkStatusMutex.Lock()
 			}
 		}
 		// return the overall drink status to the pump
-		drinkStatusMutex.Unlock()
 		currentChannel <- models.PumpStatus{
 			IngredientEmpty: drinkStatus.IngredientEmpty,
 			CurrentlyServing: drinkStatus.CurrentlyServing,
 		}
-		drinkStatusMutex.Lock()
 	}
 	// When the channel has been closed, delete it from the channel map
-	runningPumpsMutex.Lock()
 	delete(runningPumps, pump.ID)
 
 	// if there are no more channels in the map, we are no longer serving
 	if (len(runningPumps) == 0) {
 		drinkStatus.CurrentlyServing = false
 	}
-	runningPumpsMutex.Unlock()
 
 	return nil
 }
