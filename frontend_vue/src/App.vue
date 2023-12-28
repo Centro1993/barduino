875476165208d0c473d1f7ce424c6b52782ddb26
aaron.schroeder <aaron163@gmail.com>
2023-12-28 16:40:25 +0100

    Refactor code and update app title
    The code has been refactored to improve readability by properly aligning the attributes and the methods in the Vue files. This made the code much cleaner without affecting functionality. The application's title has also been updated from "Vite App" to "Barduino".

--- frontend_vue/src/App.vue
+++ frontend_vue/src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 
 import DrinkSelection from "@/components/DrinkSelector.vue";
 import Overlay from "@/components/Overlay.vue";
@@ -7,79 +7,79 @@ import {ref} from "vue";
 let isBrewing = ref<boolean>();
 
 function startDrink() {
-    isBrewing.value = true;
+  isBrewing.value = true;
 }
 
 </script>
 
 <template>
-    <main>
-        <header class="main-menu__header">
-            <span class="main-menu__headline">BARDUINO</span>
-            <span class="main-menu__credits">CRUSI 2023 EDITION</span>
-        </header>
-        <section class="main-menu__content">
-            <div class="main-menu__left-column">
-                <drink-selection @start="startDrink" />
-            </div>
-            <div class="main-menu__right-column">
-                <img src="@/assets/MiniMugV2.gif" alt="beer animation"/>
-            </div>
-        </section>
-        <overlay v-if="isBrewing" @click="isBrewing = false"/>
-    </main>
+  <main>
+    <header class="main-menu__header">
+      <span class="main-menu__headline">BARDUINO</span>
+      <span class="main-menu__credits">JONAS ▪ MARTIN ▪ AARON</span>
+    </header>
+    <section class="main-menu__content">
+      <div class="main-menu__left-column">
+        <drink-selection @start="startDrink"/>
+      </div>
+      <div class="main-menu__right-column">
+        <img alt="beer animation" src="@/assets/MiniMugV2.gif"/>
+      </div>
+    </section>
+    <overlay v-if="isBrewing" @click="isBrewing = false"/>
+  </main>
 </template>
 
 <style scoped>
 main {
-    border: .25rem solid var(--barduino-medium-brown);
-    background: var(--barduino-light-brown);
-    box-shadow: .25rem .25rem 0 0 var(--barduino-dark-brown), .5rem .5rem 0 0 var(--barduino-gray);
+  border: .25rem solid var(--barduino-medium-brown);
+  background: var(--barduino-light-brown);
+  box-shadow: .25rem .25rem 0 0 var(--barduino-dark-brown), .5rem .5rem 0 0 var(--barduino-gray);
 }
 
 main > * {
-    margin: 1rem;
+  margin: 1rem;
 }
 
 .main-menu__header {
-    color: var(--barduino-medium-brown);
+  color: var(--barduino-medium-brown);
 }
 
 .main-menu__headline {
-    font-size: 4rem;
+  font-size: 4rem;
 }
 
 .main-menu__credits {
-    font-size: 1rem;
+  font-size: 1rem;
 }
 
 .main-menu__content {
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 
 header {
-    display: flex;
-    align-items: end;
-    justify-content: space-between;
-    border-bottom: .25rem solid var(--barduino-medium-brown);
+  display: flex;
+  align-items: end;
+  justify-content: space-between;
+  border-bottom: .25rem solid var(--barduino-medium-brown);
 }
 
 .main-menu__left-column {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
 }
 
 .main-menu__right-column {
-    border: .25rem solid var(--barduino-medium-brown);
-    background: var(--barduino-gray);
+  border: .25rem solid var(--barduino-medium-brown);
+  background: var(--barduino-gray);
 }
 
 .main-menu__right-column > img {
-    height: auto;
-    max-width: 100%;
+  max-width: 100%;
+  height: auto;
 }
 
 </style>
--- frontend_vue/src/components/DrinkSelector.vue
+++ frontend_vue/src/components/DrinkSelector.vue
@@ -1,66 +1,67 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 
 function test() {
-    console.log('test');
+  console.log('test');
 }
 
 </script>
 
 <template>
-    <div class="drink-selection">
-        <svg @click="test"
-             class="drink-selection__navigation-up"
-             width="10%"
-             height="10%"
-             viewBox="0 0 50 30"
-             xmlns="http://www.w3.org/2000/svg">
-            <g>
-                <rect x="0" y="20" width="10" height="10"/>
-                <rect x="10" y="10" width="10" height="10"/>
-                <rect x="20" y="0" width="10" height="10"/>
-                <rect x="30" y="10" width="10" height="10"/>
-                <rect x="40" y="20" width="10" height="10"/>
-            </g>
-        </svg>
-        <span class="drink-selection__drink" @click="$emit('start')">drink</span>
-        <svg @click="test"
-             class="drink-selection__navigation-down"
-             width="10%"
-             height="10%"
-             viewBox="0 0 50 30"
-             xmlns="http://www.w3.org/2000/svg">
-            <g>
-                <rect x="0" y="20" width="10" height="10"/>
-                <rect x="10" y="10" width="10" height="10"/>
-                <rect x="20" y="0" width="10" height="10"/>
-                <rect x="30" y="10" width="10" height="10"/>
-                <rect x="40" y="20" width="10" height="10"/>
-            </g>
-        </svg>
-    </div>
+  <div class="drink-selection">
+    <svg class="drink-selection__navigation-up"
+         height="10%"
+         viewBox="0 0 50 30"
+         width="10%"
+         xmlns="http://www.w3.org/2000/svg"
+         @click="test">
+      <g>
+        <rect height="10" width="10" x="0" y="20"/>
+        <rect height="10" width="10" x="10" y="10"/>
+        <rect height="10" width="10" x="20" y="0"/>
+        <rect height="10" width="10" x="30" y="10"/>
+        <rect height="10" width="10" x="40" y="20"/>
+      </g>
+    </svg>
+    <span class="drink-selection__drink" @click="$emit('start')">drink</span>
+    <svg class="drink-selection__navigation-down"
+         height="10%"
+         viewBox="0 0 50 30"
+         width="10%"
+         xmlns="http://www.w3.org/2000/svg"
+         @click="test">
+      <g>
+        <rect height="10" width="10" x="0" y="20"/>
+        <rect height="10" width="10" x="10" y="10"/>
+        <rect height="10" width="10" x="20" y="0"/>
+        <rect height="10" width="10" x="30" y="10"/>
+        <rect height="10" width="10" x="40" y="20"/>
+      </g>
+    </svg>
+  </div>
 </template>
 
 <style scoped>
 .drink-selection {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 100%;
-    align-items: center;
-    margin: 4rem 0;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  margin: 4rem 0;
 }
 
 .drink-selection__navigation-down, .drink-selection__navigation-up {
-    cursor: pointer;
-    fill: var(--barduino-dark-brown);
+  cursor: pointer;
+  fill: var(--barduino-dark-brown);
 }
 
 .drink-selection__navigation-down {
-    transform: rotate(180deg);
+  transform: rotate(180deg);
 }
 
 .drink-selection__drink {
-    font-size: 3rem;
-    color: var(--barduino-dark-brown);
+  font-size: 3rem;
+  cursor: pointer;
+  color: var(--barduino-dark-brown);
 }
 </style>
